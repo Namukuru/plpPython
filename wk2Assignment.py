8112b056297d52a44c1c30f9db76920423569179
Lauryn Namukuru <namukurulauryn@gmail.com>
2024-02-29 11:49:09 +0300

    Week 2 Python Assignment.

---
+++ wk2Assignment.py
@@ -0,0 +1,13 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list[1] = 15
+my_list2 = [50, 60, 70]
+joined_list = my_list + my_list2
+my_list.remove(40)
+my_list.sort()
+index = my_list.index(30)
+print(index)
+print(my_list)
