663a2363dc20c07874116b383adaa7ef7a159bc0
Lauryn Namukuru <namukurulauryn@gmail.com>
2024-02-29 12:16:40 +0300

    addition of  user input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = str(input("Enter your age: "))
+location = input("Enter your location: ")
+message = "Hello "+name+", you are "+age+" years old and live in "+location
+print(message)
